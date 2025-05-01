b756e0c589ee7d548330ceb5b1176e8b9ea60289
matolala <martinsadagbusi@yahoo.com>
2025-05-01 12:23:42 -0500

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -105,7 +105,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/martins.pem")
+    private_key = file("C:/Users/marti/Downloads/martins.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 
