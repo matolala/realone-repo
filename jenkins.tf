c458d58901e5b1b0c455ad7b1dc3f99732894e23
matolala <martinsadagbusi@yahoo.com>
2025-05-01 13:45:10 -0500

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -99,33 +99,31 @@ resource "aws_instance" "ec2_instance" {
 
 
 # an empty resource block
-resource "null_resource" "name" {
+resource "null_resource" "jenkins_provisioner" {
+  count = 0  # disables this block
 
-  # ssh into the ec2 instance 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/martins.pem")
-    host        = aws_instance.ec2_instance.public_ip
+    private_key = file("C:/Users/marti/Downloads/martins.pem")
+    host        = aws_instance.jenkins_instance.public_ip
   }
 
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
   provisioner "file" {
     source      = "install_jenkins.sh"
     destination = "/tmp/install_jenkins.sh"
   }
 
-  # set permissions and run the install_jenkins.sh file
   provisioner "remote-exec" {
     inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
+      "sudo chmod +x /tmp/install_jenkins.sh",
+      "sh /tmp/install_jenkins.sh"
     ]
   }
 
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
+  depends_on = [aws_instance.jenkins_instance]
 }
+
 
 
 # print the url of the jenkins server
