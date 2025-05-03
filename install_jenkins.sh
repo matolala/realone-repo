962bb38f1e027379222adb36aec5c79e6f56666f
matolala <martinsadagbusi@yahoo.com>
2025-05-03 07:59:44 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -2,7 +2,7 @@
 
 # Update system and install Java
 sudo apt update -y
-sudo apt install openjdk-11-jdk -y
+sudo apt install openjdk-17-jdk -y
 
 # Add Jenkins key and repo
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
